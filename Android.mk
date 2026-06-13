eeea848101aa0f7eb2c76fbbf6433cbc0764e9b3
newestzdn <127198882+newestzdn@users.noreply.github.com>
2026-06-13 01:38:51 +0000

    a

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),X6882)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,ba82f4b20038c7688e80051a2c76471b740d07c9)
+$(call add-radio-file-sha1-checked,radio/gz.img,6f38e6964e71de341c0b6c47a718f209dab4463d)
+$(call add-radio-file-sha1-checked,radio/lk.img,8426658bf3e884b9f1965d1139264005b81b08d1)
+$(call add-radio-file-sha1-checked,radio/logo.img,e3075cd8e2848d92ad252f038681a9f18a89c688)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,c18d4522b1df88e308f895bff62c0804475b7158)
+$(call add-radio-file-sha1-checked,radio/md1img.img,cda37137e4254998ea4752723729a8f82d258cf4)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,cca3c8454a6d2de62d28c9d81e4dd651b69fdea5)
+$(call add-radio-file-sha1-checked,radio/scp.img,66a5adeeb7c7a6747a5c49d3d65dcae70c4b6ce5)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,606ff590ed52097a2d504ba56884c232591fd8af)
+$(call add-radio-file-sha1-checked,radio/sspm.img,71bba64bcc615ddf752f7fbeb50f8a255f413391)
+$(call add-radio-file-sha1-checked,radio/tee.img,5d20e6987b40d2d3fb534f57c72cc641996c1d42)
+$(call add-radio-file-sha1-checked,radio/tkv.img,ebfd7f6104f3ebf40726ce65322dd86695562fcb)
+
+endif
