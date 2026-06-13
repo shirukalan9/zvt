42705b1267e742a78831f26fe8be635c94d8b244
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-06-13 20:12:11 +0700

    y

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -52,5 +52,6 @@ rm -rf build/make
 git clone --depth 1 -b lineage-23.2 https://github.com/shirukalan9/zbuilt build/make
 rm -rf external/scudo
 git clone --depth 1 -b patch-1 https://github.com/shirukalan9/android_external_scudo external/scudo
-
+rm -rf art
+git clone --depth 1 https://github.com/shirukalan9/android_art art
 
